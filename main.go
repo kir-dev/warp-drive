370f2b83ca696b548b2da0a85959f00fc162c4c3
Tamas Michelberger <tomi.michel@gmail.com>
2014-04-07 00:25:17 +0200

    bit of refactoring in main.go
    new command line option: -log for setting the log file's path

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"os"
 )
 
 var (
@@ -23,11 +24,21 @@ func main() {
 	env0 := flag.String("env", "dev", "possible values: dev, prod, test")
 	configPath := flag.String("config", "config/config.json", "path of the confile file")
 	port := flag.String("port", ":8080", "port to run on")
+	logfile := flag.String("log", "", "path to the logfile; if left empty the app will log to stdout")
 	flag.Parse()
 
+	setLogOutput(*logfile)
 	env = environment(*env0)
 	config = loadConfiguration(*configPath)
 
+	initDb()
+	createPreparedStmts()
+
+	log.Printf("Started on %s port in %s mode", *port, env)
+	http.ListenAndServe(*port, nil)
+}
+
+func initDb() {
 	var err error
 	db, err = sql.Open("postgres", fmt.Sprintf("dbname=%s port=%d user=%s password=%s", config.Db.Name, config.Db.Port, config.Db.User, config.Db.Pass))
 	db.SetMaxIdleConns(config.Db.Pool)
@@ -35,6 +46,10 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func createPreparedStmts() {
+	var err error
 
 	imageInsertStmt, err = db.Prepare(ImageInsertSql)
 	if err != nil {
@@ -55,7 +70,15 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
 
-	log.Printf("Started on %s port", *port)
-	http.ListenAndServe(*port, nil)
+func setLogOutput(logfile string) {
+	if logfile != "" {
+		file, err := os.OpenFile(logfile, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			panic("cannot open file for logging")
+		}
+		log.SetOutput(file)
+	}
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
 }
