a5cb360c89d1a90efa795c63354e6a1c3127b088
Tamas Michelberger <tomi.michel@gmail.com>
2014-05-04 23:51:27 +0200

    added config options for oauth login (#9)
    Makefile task to generate session secret

--- config.go
+++ config.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
+	"code.google.com/p/goauth2/oauth"
 	"encoding/json"
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"strings"
 )
 
 type dbConfiguration struct {
@@ -21,10 +24,36 @@ type configuration struct {
 	User          string
 	Password      string
 	Db            dbConfiguration
+	SessionSecret string
+	ClientId      string // auth.sch client id
+	ClientSecret  string // auth.sch client secret
+	Secure        bool   // using https
+	GroupId       int    // id of the group that is allowed to upload images. if <= 0 everybody allowed
 }
 
 func (c *configuration) absolutePath(path string) string {
 	return filepath.Join(c.UploadPath, path)
+}
+
+func (c *configuration) oauth() *oauth.Config {
+	var redirect bytes.Buffer
+
+	redirect.WriteString("http")
+	if c.Secure {
+		redirect.WriteString("s")
+	}
+	redirect.WriteString("://")
+	redirect.WriteString(strings.TrimRight(c.ServerAddress, "/"))
+	redirect.WriteString("/auth")
+
+	return &oauth.Config{
+		ClientId:     c.ClientId,
+		ClientSecret: c.ClientSecret,
+		Scope:        "basic eduPersonEntitlement",
+		TokenURL:     "https://auth.sch.bme.hu/oauth2/token",
+		AuthURL:      "https://auth.sch.bme.hu/site/login",
+		RedirectURL:  redirect.String(),
+	}
 }
 
 func loadConfiguration(configPath string) configuration {
@@ -41,6 +70,5 @@ func loadConfiguration(configPath string) configuration {
 	if config.User == "" || config.Password == "" {
 		log.Fatal("user and password are required in the config")
 	}
-
 	return config
 }
